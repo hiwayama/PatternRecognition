f7ac4eea53754676b8487730e5d2965f7ec0bf25
ymis1080 <iwayma1880@gmail.com>
2012-04-03 00:52:56 +0900

    add pattern number in Exp1

--- src/main/scala/exp1/Exp1.scala
+++ src/main/scala/exp1/Exp1.scala
@@ -5,7 +5,7 @@ object Exp1 {
     //数字間のハミング距離を出力
     val np = new NumberPattern
 
-    (0 to 9).map(n=>np.pattern(n.toString)).combinations(2).foreach(l=>println(np.hammingDist(l(0),l(1))))
+    (0 to 9).map(n=>(n,np.pattern(n.toString))).combinations(2).foreach(l=>println(l(0)._1+"-"+l(1)._1+":"+np.hammingDist(l(0)._2,l(1)._2)))
 
 
     def main(args:Array[String]){
