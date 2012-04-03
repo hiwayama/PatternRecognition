0302ebfc7583382cb347153ee30140f82e9741f5
ymis1080 <iwayma1880@gmail.com>
2012-04-03 14:55:08 +0900

    add output result 1(1)

--- src/main/scala/exp1/Exp1.scala
+++ src/main/scala/exp1/Exp1.scala
@@ -10,14 +10,20 @@ object Exp1 {
     
     val results = (0 to 9).map(n=>(n,np.pattern(n.toString))).combinations(2).map(l=>(l(0)._1,l(1)._1,np.hammingDist(l(0)._2,l(1)._2))).toList
     
-    //ハミング距離が最大・最小の組み合わせを表示
-    val max = results.maxBy(_._3)
-    println("max:%d (%d-%d)".format(max._3,max._1,max._2))
-    val min = results.minBy(_._3)
-    println("min:%d (%d-%d)".format(min._3,min._1,min._2))
 
-    def main(args:Array[String]){} 
+    def main(args:Array[String]){
+        //ハミング距離が最大・最小の組み合わせを表示
+        val max = results.maxBy(_._3)
+        println("max:%d (%d-%d)".format(max._3,max._1,max._2))
+        val min = results.minBy(_._3)
+        println("min:%d (%d-%d)".format(min._3,min._1,min._2))
+    
+        //ハミング距離のヒストグラムを出力
+        toHist(results.map(_._3)).toSeq.sortWith(_._1 < _._1)foreach(s=>println(s._1+":"+s._2))
 
+    } 
+
+    //ヒストグラム作成メソッド
     def toHist(lists:List[Int]):Map[Int,Int]={
         val map = Map[Int,Int]()
         lists.foreach(l=>map(l)=map.getOrElse(l,0)+1)
