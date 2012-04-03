7a0c1b041df2a9ff924d815c9d818d7be84d2eb7
ymis1080 <iwayma1880@gmail.com>
2012-04-03 21:05:35 +0900

    名前かえたー

--- src/main/scala/exp1/Exp1.scala
+++ src/main/scala/exp1/Exp1.scala
@@ -5,12 +5,12 @@ import scala.main.NumberPattern
 object Exp1 {
 
     val r = new scala.util.Random 
-    //0~9間の10C2の組合せのハミング距離を出力
     val np = NumberPattern
-    
-    val results = (0 to 9).map(n=>(n,np.pattern(n.toString))).combinations(2).map(l=>(l(0)._1,l(1)._1,np.hammingDist(l(0)._2,l(1)._2))).toList
-    
+        
     def exp11 {
+        //0~9間の10C2の組合せのハミング距離
+        val results = (0 to 9).map(n=>(n,np.pattern(n.toString))).combinations(2).map(l=>(l(0)._1,l(1)._1,np.hammingDist(l(0)._2,l(1)._2))).toList
+     
         println("1 (1)")
         //ハミング距離が最大・最小の組み合わせを表示
         val max = results.maxBy(_._3)
